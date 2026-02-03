88171aba53629178a9eb583dc1bd23a219e881d5
Tala <talaalfaleh987@gmail.com>
2026-02-03 13:32:05 +0300

    fix: reactive form submission

--- reactiveForm/src/app/app.ts
+++ reactiveForm/src/app/app.ts
@@ -1,33 +1,27 @@
-import { JsonPipe } from '@angular/common';
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ReactiveFormsModule, JsonPipe],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App implements OnInit {
+export class App {
   protected readonly title = signal('reactiveForm');
 
   // create form group
-  userForm: FormGroup= new FormGroup({
-fullName: new FormControl("",[Validators.required,Validators.minLength(3)]),
-emailId:new FormControl("",[Validators.required,Validators.email]),
-password:new FormControl("",[Validators.required,Validators.minLength(8)]),
-age: new FormControl(0,[Validators.required,Validators.min(18)]),
-acceptTerm: new FormControl(false,[Validators.required, Validators.requiredTrue]),
-
+  userForm: FormGroup = new FormGroup({
+    fullName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    emailId: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    age: new FormControl(0, [Validators.required, Validators.min(18)]),
+    acceptTerm: new FormControl(false, [Validators.required, Validators.requiredTrue]),
   });
 
-  onSubmit(){
-    console.log(this.userForm.valid);
-  alert("The Form Submitted successfully :) ");
-  }
-  ngOnInit(): void {
-    console.log(this.userForm.get('acceptTerm')?.errors);
-
+  onSubmit() {
+    if (this.userForm.valid) {
+      alert('The Form Submitted successfully');
+    }
   }
 }
